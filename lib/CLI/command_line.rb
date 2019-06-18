8b6d2e67db05475ca8ec0ffe8f6c32bc5403371a
github username <github email address>
2019-06-18 14:38:35 -0400

    added location search + return

--- lib/CLI/command_line.rb
+++ lib/CLI/command_line.rb
@@ -1,6 +1,19 @@
 
 def welcome
     puts "Welcome to HQ! Please input your username."
-    username = gets.chomp
+    name = gets.chomp
+    User.create(username: name)
     puts "Welcome back, #{username}!"
 end
+
+def get_location
+    puts "Please enter the city you'd like to work in."
+    city = gets.chomp
+    city_match = Company.where location: city
+    company_names = city_match.map do |company|
+        company.name
+    end
+    puts "Here are the companies that have offices in the city you requested:"
+    company_names
+end
+
