c11edf6526cf82425c9fecc597b2a8805b108109
github username <github email address>
2019-06-20 09:44:34 -0400

    added description column to company

--- bin/run.rb
+++ bin/run.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment.rb'
 
 
 # CSV.foreach('/Users/annamester/Development/code/module-one-final-project-guidelines-nyc-web-060319/db/organizations.csv', headers: true) do |row|
-#     Company.create(name: row[1], location: row[12])
+#     Company.create(name: row[1], location: row[12], description: row[15])
 # end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190618193623) do
+ActiveRecord::Schema.define(version: 20190620132805) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.string "description"
   end
 
   create_table "favorites", force: :cascade do |t|
